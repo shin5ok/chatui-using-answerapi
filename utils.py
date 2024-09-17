a768883b362ca28a76b5b4ec4022c87d0ae1d266
shin5ok <shin5ok@55mp.com>
2024-09-17 20:14:57 +0900

    prepare utilities to manage features easily

---
+++ utils.py
@@ -0,0 +1,34 @@
+from google.cloud import storage
+import datetime
+
+# You need a service account file, cannot use ADC auth flow
+def get_authenticated_url(gcs_path, expiration=60):
+
+    path_parts = gcs_path.replace("gs://", "").split("/", 1)
+    bucket_name = path_parts[0]
+    object_name = path_parts[1] if len(path_parts) > 1 else ""
+
+    storage_client = storage.Client()
+
+    try:
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(object_name)
+
+        url = blob.generate_signed_url(
+            version="v4",
+            expiration=datetime.timedelta(seconds=expiration),
+            method="GET"
+        )
+
+        return url
+
+    except Exception as e:
+        print(e)
+        return ""
+
+
+if __name__ == "__main__":
+    import sys
+    gcs_path = sys.argv[1]
+    url = get_authenticated_url(gcs_path)
+    print(url)
