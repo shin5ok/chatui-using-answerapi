061126d66a8b6992b41b3406776a5fabd9560fe5
shin5ok <shin5ok@55mp.com>
2024-10-03 19:48:24 +0900

    modify something

--- answer.py
+++ answer.py
@@ -40,7 +40,7 @@ def query(
         ),
         search_spec=discoveryengine.AnswerQueryRequest.SearchSpec(
             search_params=discoveryengine.AnswerQueryRequest.SearchSpec.SearchParams(
-                max_return_results=5,
+                max_return_results=3,
             )
         ),
         answer_generation_spec=discoveryengine.AnswerQueryRequest.AnswerGenerationSpec(
@@ -48,7 +48,13 @@ def query(
                 model_version=MODEL_VERSION,
             ),
             prompt_spec=discoveryengine.AnswerQueryRequest.AnswerGenerationSpec.PromptSpec(
-                preamble="箇条書きで回答してください。"
+                preamble="""
+                    Given the conversation between a user and a helpful assistant and some search results, create a final answer for the assistant.
+                    The answer should use all relevant information from the search results, not introduce any additional information, and use exactly the same words as the search results when possible.
+                    The assistant's answer should be no more than 20 sentences.
+                    The assistant's answer should be formatted as a bulleted list.
+                    Each list item should start with the "-" symbol.
+                """
             ),
             include_citations=True,
             ignore_low_relevant_content=True,
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
   
 VERTEX_AI_LOCATION = "global"
-MODEL_VERSION = "gemini-1.5-flash-001/answer_gen/v1"
+MODEL_VERSION = "gemini-1.5-flash-002/answer_gen/v1"
 
 PROJECT_ID = os.environ.get("PROJECT_ID")
 DATASTORE_ID = os.environ.get("DATASTORE_ID")
@@ -10,3 +10,4 @@ REF_PAGES = os.environ.get("REF_PAGES")
 REF_ONLY = os.environ.get("REF_ONLY")
 
 SUBJECT = os.environ.get("SUBJECT")
+
