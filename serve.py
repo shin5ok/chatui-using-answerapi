3d03c7247f1a7c02de542e201759ca184136f5c3
shin5ok <shin5ok@55mp.com>
2024-12-11 15:48:03 +0900

    revert to work

--- serve.py
+++ serve.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from chainlit.utils import mount_chainlit
 from chainlit.context import init_http_context
 
 app = FastAPI()
 
-mount_chainlit(app=app, target="main.py", path="/")
+@app.get("/")
+def _root():
+    return RedirectResponse("/chat")
+
+mount_chainlit(app=app, target="main.py", path="/chat")
