905b5f20a2ef5e5ca4cf9ade7b44fc034ff8ced2
shin5ok <shin5ok@55mp.com>
2024-12-12 16:59:39 +0900

    change the default

--- config.py
+++ config.py
@@ -15,10 +15,11 @@ SEARCH_PAGE_SIZE = os.environ.get("SEARCH_PAGE_SIZE", 5)
 SEARCH_CONTENT_LIMIT = int(os.environ.get("SEARCH_CONTENT_LIMIT", 100))
 RETRIEVAL_FILE_URL = os.environ.get("RETRIEVAL_FILE_URL")
 
-PREAMBLE = """
-Given the conversation between a user and a helpful assistant and some search results, create a final answer for the assistant.
-The answer should use all relevant information from the search results, not introduce any additional information, and use exactly the same words as the search results when possible.
-The assistant's answer should be no more than 20 sentences.
-The assistant's answer should be formatted as a bulleted list.
-Each list item should start with the "-" symbol.
-"""
+PREAMBLE = ""
+# PREAMBLE = """
+# Given the conversation between a user and a helpful assistant and some search results, create a final answer for the assistant.
+# The answer should use all relevant information from the search results, not introduce any additional information, and use exactly the same words as the search results when possible.
+# The assistant's answer should be no more than 20 sentences.
+# The assistant's answer should be formatted as a bulleted list.
+# Each list item should start with the "-" symbol.
+# """
