60efe7314dc6ba864f259effae3a9bd2ec42627f
shin5ok <shin5ok@55mp.com>
2024-12-15 22:38:54 +0900

    fix it

--- config.py
+++ config.py
@@ -5,21 +5,22 @@ MODEL_VERSION = "gemini-1.5-flash-002/answer_gen/v1"
 
 PROJECT_ID = os.environ.get("PROJECT_ID")
 DATASTORE_ID = os.environ.get("DATASTORE_ID")
+SUBJECT = os.environ.get("SUBJECT")
+
 
 REF_PAGES = os.environ.get("REF_PAGES")
 REF_ONLY = os.environ.get("REF_ONLY")
 
-SUBJECT = os.environ.get("SUBJECT")
-
-SEARCH_PAGE_SIZE = os.environ.get("SEARCH_PAGE_SIZE", 5)
+SEARCH_PAGE_SIZE = int(os.environ.get("SEARCH_PAGE_SIZE", 5))
 SEARCH_CONTENT_LIMIT = int(os.environ.get("SEARCH_CONTENT_LIMIT", 100))
 RETRIEVAL_FILE_URL = os.environ.get("RETRIEVAL_FILE_URL")
 
+
 PREAMBLE = "200文字以内で簡潔にまとめるように"
 # PREAMBLE = """
-# Given the conversation between a user and a helpful assistant and some search results, create a final answer for the assistant.
-# The answer should use all relevant information from the search results, not introduce any additional information, and use exactly the same words as the search results when possible.
-# The assistant's answer should be no more than 20 sentences.
-# The assistant's answer should be formatted as a bulleted list.
-# Each list item should start with the "-" symbol.
+# ユーザーと親切なアシスタントとの会話といくつかの検索結果に基づいて、アシスタントへの最終的な回答を作成します。
+# 回答では、検索結果の関連情報をすべて使用し、追加情報を導入せず、可能な場合は検索結果とまったく同じ単語を使用する必要があります。
+# アシスタントの回答は 20 文以内にする必要があります。
+# アシスタントの回答は、箇条書きリストとしてフォーマットする必要があります。
+# 各リスト項目は「-」記号で始まる必要があります。
 # """
